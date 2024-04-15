e00e3cc1f9021f55fbd03d8c7bc87ef6938dbd73
unknown <jbniyikiza20@gmail.com>
2024-04-15 10:36:19 +0200

    first commit

---
+++ src/Page/Home/HomeScreen/index.js
@@ -0,0 +1,23 @@
+import AboutMe from "../AboutMe";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+import Hero from "../Hero";
+import MyPortifolio from "../MyPortifolio";
+import MySkills from "../MySkills";
+import Testimonial from "../Testimonials";
+
+const Home =()=> {
+  return (
+    <>
+      <Hero />
+      <MySkills />
+      <AboutMe />
+      <MyPortifolio />
+      <Testimonial />
+      <ContactMe />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
